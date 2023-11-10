5099699da07d9d75f6a7ef5647c1b4c91491296f
Bars1k25fps <148854392+Bars1k25fps@users.noreply.github.com>
2023-11-10 16:22:56 +0500

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,44 @@
 
+let currentQuestionIndex = 0;
+
+function startGame() {
+    displayQuestion();
+}
+
+function displayQuestion() {
+    const questionContainer = document.querySelector('.container');
+    const question = questions[currentQuestionIndex];
+
+    if (question) {
+        const optionsHTML = question.options.map(option => `<button onclick="checkAnswer('${option}')">${option}</button>`).join('');
+        questionContainer.innerHTML = `
+            <h1>${question.question}</h1>
+            ${optionsHTML}
+        `;
+    } else {
+        questionContainer.innerHTML = '<h1>Игра завершена. Спасибо за участие!</h1>';
+    }
+}
+
+function checkAnswer(answer) {
+    const question = questions[currentQuestionIndex];
+    if (question && answer === question.correctAnswer) {
+        alert('Правильный ответ!');
+    } else {
+        alert('Неправильный ответ. Конец игры.');
+        currentQuestionIndex = questions.length; // Завершение игры после неверного ответа
+    }
+
+    currentQuestionIndex++;
+    displayQuestion();
+}
+var questionnow = 2;
+function nextquestion(){
+    var question1 = document.getElementById('question1');
+    var question2 =  document.getElementById('question2');
+    if(questionnow==2){
+        questionnow +=1;
+        question1.style.visibility = 'hidden';
+        question2.style.visibility = 'visible';
+    }
+}
