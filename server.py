867888dd5d3bc7ec2d8f1485e483012a3854c7ee
Bars1k25fps <148854392+Bars1k25fps@users.noreply.github.com>
2023-11-10 16:21:15 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/game')
+def game():
+    # Здесь можно добавить логику для игры
+    return render_template('game.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
